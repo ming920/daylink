5ee97cfc0221a12f02f19e5f796135ad6321a0f7
Minjae Kim <rkfsky920@gmail.com>
2026-01-14 14:47:58 +0900

    Add files via upload

---
+++ daylink/daylink/src/main/java/com/daylink/DaylinkApplication.java
@@ -0,0 +1,13 @@
+package com.daylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaylinkApplication.class, args);
+	}
+
+}
